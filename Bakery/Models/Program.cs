4424d73a9864107a659210c35d1d888efe2037b2
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 15:27:59 -0700

    refactor RingUp and Main methods to display a simple RingUp message

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -4,12 +4,11 @@ namespace Bakery
 {
   class Program
   {
-    public static string RingUp(int breadQuant, int pastryQuant)
+    public static void RingUp(int breadQuant, int pastryQuant)
     {
       int[] breadArray = Bread.BreadCost(breadQuant);
       int[] pastryArray = Pastry.PastryCost(pastryQuant);
-      string ringUpMessage = "Ring up!";
-      return ringUpMessage;
+      Console.WriteLine($"Ring Up!");
     }
 
     public static void Main()
@@ -21,7 +20,11 @@ namespace Bakery
       Console.WriteLine("Bread: $5 each; buy 2 get 1 free.");
       Console.WriteLine("Pastries: $2 each; $5 for 3.");
       Console.WriteLine("How many loaves of bread would you like to order?");
+      string breadInput= Console.ReadLine();
+      breadQuant = int.Parse(breadInput);
       Console.WriteLine("How many pastries would you like to order?");
+      string pastryInput= Console.ReadLine();
+      pastryQuant = int.Parse(pastryInput);
       RingUp(breadQuant,pastryQuant);
     }
   }
