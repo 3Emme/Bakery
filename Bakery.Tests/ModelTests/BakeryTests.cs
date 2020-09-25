e68ef0fb513bd6ead5fa4cc3747df32a6f15a752
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 11:11:03 -0700

    refactor test 1 to properly assert equality among an int[]

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -8,7 +8,8 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadCost_BuyOne_1and5()
     {      
-      Assert.AreEqual((1,5),Bread.BreadCost(1));
+      Assert.AreEqual(1,Bread.BreadCost(1)[0]);
+      Assert.AreEqual(5,Bread.BreadCost(1)[1]);
     }
   }
 }
