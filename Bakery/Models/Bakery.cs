49c5c850dc5cd895e71818576b077c79f54f40d3
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 13:11:21 -0700

    refactor BreadCost again to pass test 2 and 3. Still not passing.

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -7,23 +7,13 @@ namespace Bakery
   {
     static public int[] BreadCost(int breadQuant)
     {
-      int adjBreadQuant;
       int breadMod = breadQuant%3;
       int breadPrice = (breadQuant-breadMod)*5;
-      if (breadMod == 0)
+      int adjBreadQuant = breadQuant;
+      if (breadMod == 2)
       {
-        
+        adjBreadQuant += 1;
       }
-      else if (breadMod == 1)
-      {
-        
-      }
-      else
-      {
-        adjBreadQuant = breadQuant+(breadQuant/2);
-        // breadPrice = (breadQuant*5)-(breadQuant/2);
-      }
-      // int adjBreadQuant = breadQuant+(breadQuant/2);
       int[] result = {adjBreadQuant,breadPrice};
       return result;
     }
