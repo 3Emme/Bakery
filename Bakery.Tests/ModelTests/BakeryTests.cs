9952b9728eb270de1bbc2e0d5942fa9eec854d43
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 11:13:36 -0700

    write test 2

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -11,5 +11,12 @@ namespace Bakery.Tests
       Assert.AreEqual(1,Bread.BreadCost(1)[0]);
       Assert.AreEqual(5,Bread.BreadCost(1)[1]);
     }
+
+    [TestMethod]
+    public void BreadCost_BuyTwoGetOneFree_3and10()
+    {      
+      Assert.AreEqual(3,Bread.BreadCost(2)[0]);
+      Assert.AreEqual(10,Bread.BreadCost(2)[1]);
+    }
   }
 }
