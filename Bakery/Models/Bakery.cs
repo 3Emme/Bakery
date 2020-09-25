10124f15e1b83a5e808439eb0633743927d590f5
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 13:36:28 -0700

    remove uneeded writelines after passing tests for BreadCost

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -7,22 +7,14 @@ namespace Bakery
   {
     static public int[] BreadCost(int breadQuant)
     {
-      Console.WriteLine("breadQuant: "+breadQuant);
       int breadMod = breadQuant%3;
       int breadPrice = (breadQuant-(breadQuant/3))*5;
       int adjBreadQuant = breadQuant;
-      Console.WriteLine("breadMod: "+breadMod);
-      Console.WriteLine("breadPrice: "+breadPrice);
-      Console.WriteLine("adjBreadQuant: "+adjBreadQuant);
       if (breadMod == 2)
       {
-        Console.WriteLine("BreadMod == 2 branch activated");
         adjBreadQuant += 1;
       }
-      Console.WriteLine("adjBreadQuant: "+adjBreadQuant);
-      Console.WriteLine("breadPrice: "+breadPrice);
       int[] result = {adjBreadQuant,breadPrice};
-      Console.WriteLine("result: "+result[0]+","+result[1]);
       return result;
     }
   }
