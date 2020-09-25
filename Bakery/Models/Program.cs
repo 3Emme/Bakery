fba5fab9bf15294a940399134c2db222cea375d3
3Emme <emmettbuentiempo@gmail.com>
2020-09-25 15:55:00 -0700

    clean up duplicate code.

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -41,30 +41,11 @@ namespace Bakery
 
     public static void Main()
     {
-      // int breadQuant = 0;
-      // int pastryQuant = 0;
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("Here are today's prices:");
       Console.WriteLine("Bread: $5 each; buy 2 get 1 free.");
       Console.WriteLine("Pastries: $2 each; $5 for 3.");
       TakeOrder();
-      // Console.WriteLine("How many loaves of bread would you like to order?");
-      // string breadInput= Console.ReadLine();
-      // breadQuant = int.Parse(breadInput);
-      // Console.WriteLine("How many pastries would you like to order?");
-      // string pastryInput= Console.ReadLine();
-      // pastryQuant = int.Parse(pastryInput);
-      // RingUp(breadQuant,pastryQuant);
-      // Console.WriteLine("Would you like to make another order? Y/N");
-      // string anotherOrder= Console.ReadLine();
-      // if (anotherOrder == "Y")
-      // {
-      //   TakeOrder();
-      // }
-      // else
-      // {
-      //   Console.WriteLine("Ok, thanks for shopping at Pierre's Bakery!");
-      // }
     }
   }
 }
